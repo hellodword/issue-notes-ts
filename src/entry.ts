30cc0cbd4172d93047961cfb33d5b0448e6510ed
hellodword <46193371+hellodword@users.noreply.github.com>
2022-02-11 21:41:07 +0800

    sync

--- src/entry.ts
+++ src/entry.ts
@@ -28,7 +28,7 @@ import {
   only,
 } from './parse';
 
-import { dateFormat } from './helper';
+import { dateFormat, isURL } from './helper';
 import { visit } from './parse';
 
 import { sanitize } from 'string-sanitizer';
@@ -47,15 +47,15 @@ import { BuildVisitor } from 'unist-util-visit/complex-types';
 import { detectFile, detectLanguage } from './detect';
 import { sha1 } from './crypto';
 
-const BRANCH_NAME = 'gh-pages';
-const BASE_PATH_POSTS_NORMAL = '_posts';
-const BASE_PATH_POSTS_ARCHIVES = '_posts/archives';
-const BASE_PATH_ARCHIVES = 'archives';
-const BASE_PATH_PICTURE = 'assets/img';
-const BASE_PATH_CODE = 'assets/code';
-const PREFIX = '1970-01-01';
+export const BRANCH_NAME = 'gh-pages';
+export const BASE_PATH_POSTS_NORMAL = '_posts';
+export const BASE_PATH_POSTS_ARCHIVES = '_posts/archives';
+export const BASE_PATH_ARCHIVES = 'archives';
+export const BASE_PATH_PICTURE = 'assets/img';
+export const BASE_PATH_CODE = 'assets/code';
+export const PREFIX = '1970-01-01';
 
-const ARCHIVE_ENGINES = ['ArchiveBox', 'cairn', 'obelisk', 'rivet'];
+export const ARCHIVE_ENGINES = ['ArchiveBox', 'cairn', 'obelisk', 'rivet'];
 
 export async function entry(args: {
   context: Context;
@@ -282,7 +282,7 @@ async function deletePost(args: {
   );
 
   // issue 5 的所有都存在一个文件夹 _posts/5/1970-01-01-5-123456.md
-  const result = await getPathSha({
+  await getPathSha({
     rest: args.rest,
     owner: args.owner,
     repo: args.repo,
@@ -293,29 +293,48 @@ async function deletePost(args: {
         item.indexOf(`-${args.issueNumber}-${args.issueCommentId}.md`) !== -1
       );
     },
-  });
+  })
+    .then(async (sha) => {
+      console.log('deletePost', 'getPathSha', sha);
+      if (!sha || !sha.sha || sha.sha === '') {
+        return;
+      }
 
-  console.log('deletePost', 'getPathSha', result);
+      await args.rest.repos
+        .deleteFile({
+          owner: args.owner,
+          repo: args.repo,
+          branch: BRANCH_NAME,
+          path: `${BASE_PATH_POSTS_NORMAL}/${args.issueNumber}/${sha.name}`,
+          message: `delete ${
+            sha.name
+          } via github-actions${'\n\n'}${buildRawLink(args)}`,
+          sha: sha.sha,
+        })
+        .then((response) => {
+          console.log('deletePost', 'deleteFile', response.status);
+        });
+    })
+    .catch((reason) => {
+      console.log('deletePost', 'getPathSha', reason);
+    });
 
-  if (!result || !result.sha) {
-    return;
+  return;
+}
+
+function yargsArgToString(key: any) {
+  if (!key) {
+    return undefined;
   }
 
-  await args.rest.repos
-    .deleteFile({
-      owner: args.owner,
-      repo: args.repo,
-      branch: BRANCH_NAME,
-      path: `${BASE_PATH_POSTS_NORMAL}/${args.issueNumber}/${result.name}`,
-      message: `delete ${result.name} via github-actions${'\n\n'}${buildRawLink(
-        args,
-      )}`,
-      sha: result.sha,
-    })
-    .then((response) => {
-      console.log('deletePost', response.status);
-    });
-  return;
+  if (typeof key === 'string') {
+    return key as string;
+  }
+
+  if (typeof key === 'object' && key.length && key.length > 0) {
+    return key[0];
+  }
+  return undefined;
 }
 
 async function deleteArchive(args: {
@@ -408,6 +427,7 @@ async function createPost(args: {
                 'description <descriptions...>',
               ],
               handler: (argv: yargs.Arguments<{ descriptions?: string[] }>) => {
+                console.log('yargs.handler', 'desc', argv);
                 if (!result.descriptions) {
                   result.descriptions = [];
                 }
@@ -417,34 +437,38 @@ async function createPost(args: {
             .command({
               command: 'author [author]',
               handler: (argv: yargs.Arguments<{ author: string }>) => {
-                result.author = argv.author;
+                console.log('yargs.handler', 'author', argv);
+                result.author = yargsArgToString(argv.author);
               },
             })
             .command({
               command: ['img [link]', 'image [link]'],
               handler: async (argv: yargs.Arguments<{ link: string }>) => {
+                console.log('yargs.handler', 'img', argv);
                 if (!result.img) {
                   result.img = [];
                 }
                 result.img.push({
                   node: node,
-                  link: argv.link,
+                  link: yargsArgToString(argv.link),
                 });
               },
             })
             .command({
               command: ['jump [link]', 'jumplink [link]'],
               handler: (argv: yargs.Arguments<{ link: string }>) => {
-                result.jump = argv.link;
+                console.log('yargs.handler', 'jump', argv);
+                result.jump = yargsArgToString(argv.link);
               },
             })
             .command({
               command: ['del [link]', 'delete [link]'],
               handler: (argv: yargs.Arguments<{ link: string }>) => {
+                console.log('yargs.handler', 'del', argv);
                 if (!result.del) {
                   result.del = [];
                 }
-                result.del.push(argv.link);
+                result.del.push(yargsArgToString(argv.link));
               },
             })
             .command({
@@ -463,14 +487,15 @@ async function createPost(args: {
                   { lang: string } & { name: string } & { link: string }
                 >,
               ) => {
+                console.log('yargs.handler', 'code', argv);
                 if (!result.code) {
                   result.code = [];
                 }
                 result.code.push({
                   node: node,
-                  lang: argv.lang,
-                  name: argv.name,
-                  link: argv.link,
+                  lang: yargsArgToString(argv.lang),
+                  name: yargsArgToString(argv.name),
+                  link: yargsArgToString(argv.link),
                 });
               },
             })
@@ -498,15 +523,21 @@ async function createPost(args: {
                   } & { link: string }
                 >,
               ) => {
+                console.log('yargs.handler', 'archive', argv);
                 if (!result.archive) {
                   result.archive = [];
                 }
+                argv.link = yargsArgToString(argv.link);
+                if (!isURL.test(argv.link)) {
+                  console.log('yargs.handler', 'archive', '!isURL', argv.link);
+                  return;
+                }
+
                 result.archive.push({
-                  // node: node,
                   link: argv.link,
-                  title: argv.title,
-                  author: argv.author,
-                  date: argv.date,
+                  title: yargsArgToString(argv.title),
+                  author: yargsArgToString(argv.author),
+                  date: yargsArgToString(argv.date),
                   engine:
                     typeof argv.engine === 'string'
                       ? [argv.engine]
@@ -514,6 +545,9 @@ async function createPost(args: {
                 });
               },
             })
+            // https://yargs.js.org/docs/#api-reference-stringkey
+            // .string('_') will result in non-hyphenated arguments being interpreted as strings, regardless of whether they resemble numbers.
+            .string('_')
             .parse();
         }
 
@@ -525,7 +559,11 @@ async function createPost(args: {
   console.log('result.img', !!result.img);
   if (result.img) {
     for (let i = 0; i < result.img.length; i++) {
-      if (!result.img[i].link || result.img[i].link === '') {
+      if (
+        !result.img[i].link ||
+        result.img[i].link === '' ||
+        !isURL.test(result.img[i].link)
+      ) {
         continue;
       }
       const imgNode = u('image', {
@@ -594,7 +632,11 @@ async function createPost(args: {
   console.log('result.code', !!result.code);
   if (result.code) {
     for (let i = 0; i < result.code.length; i++) {
-      if (!result.code[i].link || result.code[i].link === '') {
+      if (
+        !result.code[i].link ||
+        result.code[i].link === '' ||
+        !isURL.test(result.code[i].link)
+      ) {
         continue;
       }
       // 优先用传参
